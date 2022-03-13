3c19b35f58d825a78c752d9b4b7130bf273744b0
umarcor <unai.martinezcorral@ehu.eus>
2022-03-13 14:32:58 +0100

    docs: use BuildTheDocs Action; add logo and favicon

--- docs/conf.py
+++ docs/conf.py
@@ -1,56 +1,80 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+from pathlib import Path
+from json import loads as json_loads
 
-# -- Path setup --------------------------------------------------------------
 
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-import sphinx_rtd_theme
+ROOT = Path(__file__).resolve().parent
+
 
 # -- Project information -----------------------------------------------------
 
-project = 'OSVVM-Utility'
-copyright = '2020, Jim Lewis Licensed under CC BY-NC-ND 4.0'
+project = 'OSVVM-Documentation'
 author = 'Jim Lewis'
+copyright = f'2022, {author} Licensed under CC BY-NC-ND 4.0'
 
-# The full version, including alpha/beta/rc tags
-release = '2020.05'
+version = '2022.02'
+release = version
 
 
 # -- General configuration ---------------------------------------------------
 
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
+master_doc = "index"
+
 extensions = [
-    "sphinx_rtd_theme",
+    "sphinx.ext.extlinks",
+    "sphinx.ext.intersphinx",
 ]
 
-# Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+numfig = True
 
 # -- Options for HTML output -------------------------------------------------
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-html_theme = 'sphinx_rtd_theme'
+html_context = {}
+ctx = ROOT / "context.json"
+if ctx.is_file():
+    html_context.update(json_loads(ctx.open("r").read()))
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+if (ROOT / "_theme").is_dir():
+    html_theme_path = ["."]
+    html_theme = "_theme"
+    html_theme_options = {
+        "logo_only": True,
+        "home_breadcrumbs": True,
+        "vcs_pageview_mode": "blob",
+    }
+    html_css_files = [
+        "theme_overrides.css",
+    ]
+else:
+    html_theme = "alabaster"
+
+html_static_path = ["_static"]
+
+html_logo = str(Path(html_static_path[0]) / "logo.png")
+html_favicon = str(Path(html_static_path[0]) / "favicon.png")
+
+htmlhelp_basename = "HDLCDoc"
+
+
+# -- Sphinx.Ext.InterSphinx --------------------------------------------------------------------------------------------
+
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3/", None),
+    "edaa": ("https://edaa-org.github.io", None),
+}
+
+
+# -- Sphinx.Ext.ExtLinks -----------------------------------------------------------------------------------------------
+
+extlinks = {
+    "wikipedia": ("https://en.wikipedia.org/wiki/%s", None),
+    "awesome": ("https://hdl.github.io/awesome/items/%s", ""),
+    "gh": ("https://github.com/%s", "gh:"),
+    "ghsharp": ("https://github.com/OSVVM/Documentation/issues/%s", "#"),
+    "ghissue": ("https://github.com/OSVVM/Documentation/issues/%s", "issue #"),
+    "ghpull": ("https://github.com/OSVVM/Documentation/pull/%s", "pull request #"),
+    "ghsrc": ("https://github.com/OSVVM/Documentation/blob/main/%s", ""),
+}
